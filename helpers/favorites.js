036d0abf2d0b015dbc6b6b41c4c6d3f319ae2965
Zac Isaacson <isaacson.zm@gmail.com>
2020-02-05 19:04:10 -0700

    Remove helper file

--- helpers/favorites.js
+++
@@ -1,8 +0,0 @@
-function formatFavorites(favorites) {
-  favorites.map(favorite => {
-    delete favorite.created_at;
-    delete favorite.updated_at;
-    return favorite;
-  })
-};
- module.exports = formatFavorites;
