875de68d29a00b0ca27013fc3f18de4fa2d470ce
Zac Isaacson <isaacson.zm@gmail.com>
2020-02-11 15:24:34 -0700

    Create migration for playlist/favorites joins table

---
+++ db/migrations/20200211144102_playlist-favorites.js
@@ -0,0 +1,23 @@
+
+exports.up = function(knex) {
+  return Promise.all([
+    knex.schema.createTable('playlists_favorites',
+     function(table) {
+      table.increments('id').primary();
+      table.integer('playlist_id').unsigned().notNullable()
+      table.foreign('playlist_id').references('id').inTable('playlists');
+
+      table.integer('favorite_id').unsigned().notNullable()
+      table.foreign('favorite_id').references('id').inTable('favorites');
+
+      table.timestamps(true, true);
+      })
+  ])
+};
+
+
+exports.down = function(knex) {
+  return Promise.all([
+    knex.schema.dropTable('playlists_favorites')
+  ])
+};
