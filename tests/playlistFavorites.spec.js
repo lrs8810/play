6c1b42274b8ce4b8e801155761aea5e98fd5aa4b
Zac Isaacson <isaacson.zm@gmail.com>
2020-02-12 08:19:24 -0700

    Refacter happy path spec
    Co-Authored-By: Laura Schulz <lrs8810@gmail.com>

--- tests/playlistFavorites.spec.js
+++ tests/playlistFavorites.spec.js
@@ -9,25 +9,31 @@ const database = require('knex')(configuration);
 describe('Test it can add favorites to playlists_favorites endpoint', () => {
   beforeEach(async () => {
     await database.raw('truncate table playlists_favorites cascade')
+    await database.raw('truncate table playlists cascade')
+    await database.raw('truncate table favorites cascade')
   });
   afterEach(async () => {
     await database.raw('truncate table playlists_favorites cascade');
+    await database.raw('truncate table playlists cascade');
+    await database.raw('truncate table favorites cascade');
   });
 
   describe('POST /api/v1/playlists/:id/favorites/:id', () => {
     it('happy path', async () => {
       let playlist = {
-        id: 1,
+        id: 5,
         title: 'Roadtrip'
       }
 
       let favorite_1 = {
-        title: "We Will Rock You",
+        id: 75,
+        title: "Bohemian Rhapsody",
         artistName: "Queen",
         genre: "Awesome Rock",
         rating: 80
       }
       let favorite_2 = {
+        id: 80,
         title: "You Shook Me All Night Long",
         artistName: "ACDC",
         genre: "Rock",
@@ -37,12 +43,11 @@ describe('Test it can add favorites to playlists_favorites endpoint', () => {
       await database('favorites').insert(favorite_1)
       await database('favorites').insert(favorite_2)
       const res = await request(app)
-        .post('/api/v1/playlists/1/favorites/7')
-        .send({})
+        .post('/api/v1/playlists/5/favorites/75')
 
       expect(res.statusCode).toBe(201);
       expect(res.body).toHaveProperty('Success')
-      expect(res.body.success).toBe(`${favorite_1.title} has been added to ${playlist.title}!`)
+      expect(res.body.Success).toBe(`${favorite_1.title} has been added to ${playlist.title}!`)
     })
   })
 })
